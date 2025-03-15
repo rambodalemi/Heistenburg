1ada66c481257cb90711218d551e5347f59fc99d
Rambod <rambod.alemi87@gmail.com>
2025-03-15 21:09:26 +0100

    removed paypal

--- app/(app)/api/checkout/route.ts
+++ app/(app)/api/checkout/route.ts
@@ -28,7 +28,7 @@ export async function POST(req: NextRequest) {
     );
 
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card", "paypal"],
+      payment_method_types: ["card"],
       line_items: lineItems,
       mode: "payment",
       success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
